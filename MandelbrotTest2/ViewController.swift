2f12b4e46629c4d4158c45b20fb08f10f27c1d97
Mike O'Connell <guitarmike1966@gmail.com>
2020-03-31 18:40:14 -0500

    Initial Commit

---
+++ MandelbrotTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MandelbrotTest2
+//
+//  Created by Michael O'Connell on 3/31/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MandelbrotTest2Tests/MandelbrotTest2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MandelbrotTest2Tests.swift
+//  MandelbrotTest2Tests
+//
+//  Created by Michael O'Connell on 3/31/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import XCTest
+@testable import MandelbrotTest2
+
+class MandelbrotTest2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
